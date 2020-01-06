5e5ab5be0529c4e72ee3660ab8387778a5870269
April <aprilyoung010@gmail.com>
2020-01-06 15:58:57 +0800

    chore: add read me

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # RadarGraphDemo
+
+A simple radar graph 
+
+![](radar.png)
+
+#### How to use it?
+
+```objc
+RadarGraphView *radar = [[RadarGraphView alloc] initWithTitles:@[@"aaa", @"bbb", @"ccc", @"ddd", @"eee",@"aaa", @"bbb", @"ccc", @"ddd", @"eee"] titleFont:15];
+radar.radius = 100;
+radar.values = @[@24, @44, @55, @67, @90, @24, @44, @55, @67, @90];
+radar.titleStrColor = [UIColor blackColor];
+radar.lineColor = [UIColor orangeColor];
+    
+radar.backgroundColor = [UIColor whiteColor];
+radar.frame = CGRectInset(self.view.bounds, 0, 50);
+radar.backgroundColor = [UIColor whiteColor];
+
+[self.view addSubview:radar];
+```
+
--- RadarGraphDemo/ViewController.m
+++ RadarGraphDemo/ViewController.m
@@ -18,18 +18,17 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    RadarGraphView *radar = [[RadarGraphView alloc] initWithTitles:@[@"aaa", @"bbb", @"ccc", @"ddd", @"eee",@"aaa", @"bbb", @"ccc", @"ddd", @"eee"] titleFont:20];
+    RadarGraphView *radar = [[RadarGraphView alloc] initWithTitles:@[@"aaa", @"bbb", @"ccc", @"ddd", @"eee",@"aaa", @"bbb", @"ccc", @"ddd", @"eee"] titleFont:15];
     radar.radius = 100;
+    radar.values = @[@24, @44, @55, @67, @90, @24, @44, @55, @67, @90];
+    radar.titleStrColor = [UIColor blackColor];
+    radar.lineColor = [UIColor orangeColor];
+    
     radar.backgroundColor = [UIColor whiteColor];
     radar.frame = CGRectInset(self.view.bounds, 0, 50);
-    
-    radar.backgroundColor = [UIColor systemTealColor];
+    radar.backgroundColor = [UIColor whiteColor];
+
     [self.view addSubview:radar];
-    
-    radar.titleStrColor = [UIColor greenColor];
-    radar.lineColor = [UIColor orangeColor];
-       
-    
     self.radar = radar;
 }
 
